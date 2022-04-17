66a05ff68e1f7664b4c9f4f637221052c7655a73
Yulia Melkina <52936292+koluchikrot@users.noreply.github.com>
2022-04-18 00:23:45 +0300

    Initial Commit

---
+++ MiemInvest/MiemInvestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiemInvestApp.swift
+//  MiemInvest
+//
+//  Created by Юлия Мелькина on 18.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MiemInvestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
